d938aa0f1a0f367f2b7283969876ce3ac5233f2f
duanlang119 <gordonyaogz@gmail.com>
2013-11-20 14:34:21 +0800

    'Validation!'

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,6 +15,6 @@ Ruby is the fastest growing and most exciting dynamic language
 out there. If you need to get working programs delivered fast,
 you should add Ruby to your toolbox.
 </p>},
-image_url: 'ruby.jpg',
+image_url: 'rails.png',
 price: 49.95)
 # . . .
